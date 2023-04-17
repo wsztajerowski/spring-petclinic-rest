d16010a7a8d09a81a809b88f3a1de7373fe607c2
Wiktor-Sztajerowski <wiktor.sztajerowski@gmail.com>
2023-04-17 22:48:49 +0200

    fix JPA saving issue

--- src/main/java/org/springframework/samples/petclinic/model/Vet.java
+++ src/main/java/org/springframework/samples/petclinic/model/Vet.java
@@ -35,7 +35,7 @@ import java.util.*;
 @Table(name = "vets")
 public class Vet extends Person {
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
         inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties;
